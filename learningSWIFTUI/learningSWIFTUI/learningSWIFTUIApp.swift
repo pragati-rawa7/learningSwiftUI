4edcf3777c59bbef6782212cc58ad8e13b8edd25
Pragati Rawat <pragati.rawa7t@gmail.com>
2024-01-03 13:50:45 +0530

    Project Setup for Learning SwiftUI
    1. Repo Integration
    2. Created empty Xcode ios app for swift ui learning
    3. Basic code commit for initial setup

---
+++ learningSWIFTUI/learningSWIFTUI/learningSWIFTUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningSWIFTUIApp.swift
+//  learningSWIFTUI
+//
+//  Created by Pragati RAWAT on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct learningSWIFTUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
